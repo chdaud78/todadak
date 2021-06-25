94c158dff06ad0ba0d49fba87a6517fa4403e223
chdaud78 <qkrri56@naver.com>
2021-06-25 14:47:02 +0900

    [ENH] Edit Public Path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/' : '/',
+    ? '/todadak/'
+    : '/',
     outputDir: 'docs',
     css: {
         loaderOptions: {
